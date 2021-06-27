e00a1270ea00b58801a760ba0e82bdd0b823ffb6
AS008BA <ashish.bankar@pb.com>
2021-06-27 20:18:08 +0530

    immutability

--- src/main/java/com/immutability/ImmutableAddress.java
+++ src/main/java/com/immutability/ImmutableAddress.java
@@ -1,2 +1,24 @@
-package com.immutability;public class Address {
+package com.immutability;
+
+public class ImmutableAddress {
+    private String street;
+
+    public ImmutableAddress(String street) {
+        this.street = street;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    @Override
+    public String toString() {
+        return "ImmutableAddress{" +
+                "street='" + street + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/immutability/ImmutableEmployee.java
+++ src/main/java/com/immutability/ImmutableEmployee.java
@@ -1,2 +1,29 @@
-package com.immutability;public class ImmutableEmployee {
+package com.immutability;
+
+public final class ImmutableEmployee {
+    private final int id;
+    private final ImmutableAddress immutableAddress;
+
+    public ImmutableEmployee(int id, ImmutableAddress immutableAddress) {
+        this.id = id;
+        this.immutableAddress = immutableAddress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ImmutableAddress getImmutableAddress() {
+        return new ImmutableAddress(this.immutableAddress.getStreet());
+        //return new ImmutableAddress(this.immutableAddress.getStreet());
+    }
+
+    public static void main(String[] args) {
+        ImmutableAddress address = new ImmutableAddress("aundh");
+        ImmutableEmployee e = new ImmutableEmployee(1, address);
+        System.out.println(e.getImmutableAddress().getStreet());
+        e.getImmutableAddress().setStreet("baner");
+        System.out.println(e.getImmutableAddress().getStreet());
+
+    }
 }
